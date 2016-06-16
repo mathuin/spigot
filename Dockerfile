4670ea65211dabf0dbcf6ee5ae30d332e7da392f
Jack Twilley <mathuin@gmail.com>
2016-06-15 21:13:58 -0700

    Minor tweaks.
    The Dockerfile wasn't exactly best-practices.
    
    Also, the script did some annoying things.

--- Dockerfile
+++ Dockerfile
@@ -3,24 +3,34 @@ MAINTAINER AshDev <ashdevfr@gmail.com>
 
 ENV DEBIAN_FRONTEND noninteractive
 
-ADD ./lib/apt/sources.list /etc/apt/sources.list
-RUN apt-get update -y; apt-get upgrade -y
-RUN apt-get install -y software-properties-common
+COPY ./lib/apt/sources.list /etc/apt/
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+  software-properties-common \
+  && rm -rf /var/lib/apt/lists/*
 RUN add-apt-repository ppa:webupd8team/java -y
-RUN apt-get update -y; apt-get upgrade -y
 RUN echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-RUN apt-get install -y curl oracle-java8-installer oracle-java8-set-default supervisor pwgen
-RUN apt-get update && apt-get install -y wget git && apt-get clean all
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+  curl \
+  oracle-java8-installer \
+  oracle-java8-set-default \
+  supervisor \
+  pwgen \
+  wget \
+  git \
+  && rm -rf /var/lib/apt/lists/*
 
-#default directory for SPIGOT-server
+# Data volume for Minecraft
 ENV SPIGOT_HOME /minecraft
 
-RUN useradd -s /bin/bash -d /minecraft -m minecraft
+# Location for static files
+ENV STATIC_DIR /static
 
-ADD ./lib/minecraft/opts.txt /usr/local/etc/minecraft/opts.txt
-ADD ./lib/minecraft/white-list.txt /usr/local/etc/minecraft/white-list.txt
-ADD ./lib/minecraft/server.properties /usr/local/etc/minecraft/server.properties
-ADD ./lib/scripts/spigot_init.sh /spigot_init.sh
+RUN useradd -s /bin/bash -d $SPIGOT_HOME -m minecraft
+
+COPY ./lib/minecraft/* $STATIC_DIR/
+COPY ./lib/scripts/spigot_init.sh /
 
 RUN chmod +x /spigot_init.sh
 
@@ -33,6 +43,7 @@ ENV UID=1000
 ENV MOTD A Minecraft Server Powered by Spigot & Docker
 ENV REV latest
 ENV JVM_OPTS -Xmx1024M -Xms1024M
+# replace these with defaults in spigot_init.sh
 ENV LEVEL=world PVP=true VDIST=10 OPPERM=4 NETHER=true FLY=false MAXBHEIGHT=256 NPCS=true WLIST=false ANIMALS=true HC=false ONLINE=true RPACK='' DIFFICULTY=3 CMDBLOCK=false MAXPLAYERS=20 MONSTERS=true STRUCTURES=true SPAWNPROTECTION=16
 
 #ENV DYNMAP=true ESSENTIALS=false PERMISSIONSEX=false CLEARLAG=false
--- lib/scripts/spigot_init.sh
+++ lib/scripts/spigot_init.sh
@@ -83,115 +83,115 @@ fi
 
 if [ ! -f /$SPIGOT_HOME/white-list.txt ]
 then
-    cp /usr/local/etc/minecraft/white-list.txt /$SPIGOT_HOME/
+    cp $STATIC_DIR/white-list.txt /$SPIGOT_HOME/
 fi
 
 if [ ! -f /$SPIGOT_HOME/server.properties ]
 then
-  cp /usr/local/etc/minecraft/server.properties /$SPIGOT_HOME/
+  cp $STATIC_DIR/server.properties /$SPIGOT_HOME/
+fi
 
-  if [ -n "$MOTD" ]; then
-    sed -i "/motd\s*=/ c motd=$MOTD" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$MOTD" ]; then
+  sed -i "/motd\s*=/ c motd=$MOTD" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$LEVEL" ]; then
-    sed -i "/level-name\s*=/ c level-name=$LEVEL" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$LEVEL" ]; then
+  sed -i "/level-name\s*=/ c level-name=$LEVEL" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$SEED" ]; then
-    sed -i "/level-seed\s*=/ c level-seed=$SEED" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$SEED" ]; then
+  sed -i "/level-seed\s*=/ c level-seed=$SEED" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$PVP" ]; then
-    sed -i "/pvp\s*=/ c pvp=$PVP" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$PVP" ]; then
+  sed -i "/pvp\s*=/ c pvp=$PVP" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$VDIST" ]; then
-    sed -i "/view-distance\s*=/ c view-distance=$VDIST" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$VDIST" ]; then
+  sed -i "/view-distance\s*=/ c view-distance=$VDIST" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$OPPERM" ]; then
-    sed -i "/op-permission-level\s*=/ c op-permission-level=$OPPERM" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$OPPERM" ]; then
+  sed -i "/op-permission-level\s*=/ c op-permission-level=$OPPERM" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$NETHER" ]; then
-    sed -i "/allow-nether\s*=/ c allow-nether=$NETHER" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$NETHER" ]; then
+  sed -i "/allow-nether\s*=/ c allow-nether=$NETHER" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$FLY" ]; then
-    sed -i "/allow-flight\s*=/ c allow-flight=$FLY" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$FLY" ]; then
+  sed -i "/allow-flight\s*=/ c allow-flight=$FLY" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$MAXBHEIGHT" ]; then
-    sed -i "/max-build-height\s*=/ c max-build-height=$MAXBHEIGHT" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$MAXBHEIGHT" ]; then
+  sed -i "/max-build-height\s*=/ c max-build-height=$MAXBHEIGHT" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$NPCS" ]; then
-    sed -i "/spawn-npcs\s*=/ c spawn-npcs=$NPCS" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$NPCS" ]; then
+  sed -i "/spawn-npcs\s*=/ c spawn-npcs=$NPCS" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$WLIST" ]; then
-    sed -i "/white-list\s*=/ c white-list=$WLIST" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$WLIST" ]; then
+  sed -i "/white-list\s*=/ c white-list=$WLIST" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$ANIMALS" ]; then
-    sed -i "/spawn-animals\s*=/ c spawn-animals=$ANIMALS" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$ANIMALS" ]; then
+  sed -i "/spawn-animals\s*=/ c spawn-animals=$ANIMALS" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$HC" ]; then
-    sed -i "/hardcore\s*=/ c hardcore=$HC" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$HC" ]; then
+  sed -i "/hardcore\s*=/ c hardcore=$HC" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$ONLINE" ]; then
-    sed -i "/online-mode\s*=/ c online-mode=$ONLINE" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$ONLINE" ]; then
+  sed -i "/online-mode\s*=/ c online-mode=$ONLINE" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$RPACK" ]; then
-    sed -i "/resource-pack\s*=/ c resource-pack=$RPACK" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$RPACK" ]; then
+  sed -i "/resource-pack\s*=/ c resource-pack=$RPACK" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$DIFFICULTY" ]; then
-    sed -i "/difficulty\s*=/ c difficulty=$DIFFICULTY" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$DIFFICULTY" ]; then
+  sed -i "/difficulty\s*=/ c difficulty=$DIFFICULTY" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$CMDBLOCK" ]; then
-    sed -i "/enable-command-block\s*=/ c enable-command-block=$CMDBLOCK" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$CMDBLOCK" ]; then
+  sed -i "/enable-command-block\s*=/ c enable-command-block=$CMDBLOCK" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$MAXPLAYERS" ]; then
-    sed -i "/max-players\s*=/ c max-players=$MAXPLAYERS" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$MAXPLAYERS" ]; then
+  sed -i "/max-players\s*=/ c max-players=$MAXPLAYERS" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$MONSTERS" ]; then
-    sed -i "/spawn-monsters\s*=/ c spawn-monsters=$MONSTERS" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$MONSTERS" ]; then
+  sed -i "/spawn-monsters\s*=/ c spawn-monsters=$MONSTERS" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$STRUCTURES" ]; then
-    sed -i "/generate-structures\s*=/ c generate-structures=$STRUCTURES" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$STRUCTURES" ]; then
+  sed -i "/generate-structures\s*=/ c generate-structures=$STRUCTURES" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$SPAWNPROTECTION" ]; then
-    sed -i "/spawn-protection\s*=/ c spawn-protection=$SPAWNPROTECTION" /$SPIGOT_HOME/server.properties
-  fi
+if [ -n "$SPAWNPROTECTION" ]; then
+  sed -i "/spawn-protection\s*=/ c spawn-protection=$SPAWNPROTECTION" /$SPIGOT_HOME/server.properties
+fi
 
-  if [ -n "$MODE" ]; then
-    case ${MODE,,?} in
-      0|1|2|3)
-        ;;
-      s*)
-        MODE=0
-        ;;
-      c*)
-        MODE=1
-        ;;
-      *)
-        echo "ERROR: Invalid game mode: $MODE"
-        exit 1
-        ;;
-    esac
+if [ -n "$MODE" ]; then
+  case ${MODE,,?} in
+    0|1|2|3)
+      ;;
+    s*)
+      MODE=0
+      ;;
+    c*)
+      MODE=1
+      ;;
+    *)
+      echo "ERROR: Invalid game mode: $MODE"
+      exit 1
+      ;;
+  esac
 
-    sed -i "/gamemode\s*=/ c gamemode=$MODE" /$SPIGOT_HOME/server.properties
-  fi
+  sed -i "/gamemode\s*=/ c gamemode=$MODE" /$SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$OPS" -a ! -e /$SPIGOT_HOME/ops.txt.converted ]; then
