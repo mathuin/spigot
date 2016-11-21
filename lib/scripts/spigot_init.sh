5893dab1b60bbb1e09c8002f81d4663d26758cd1
Jack Twilley <mathuin@gmail.com>
2016-11-20 17:33:52 -0800

    Cleaned up script.
    Removed silly leading slashes.  Started using EssentialsX.  Discovered
    that CREEPERBLOCKDISABLE can't ever work on the first boot, because the
    configuration file doesn't yet exist.

--- lib/scripts/spigot_init.sh
+++ lib/scripts/spigot_init.sh
@@ -2,10 +2,10 @@
 set -e
 usermod --uid $UID minecraft
 
-if [ ! -e /$SPIGOT_HOME/eula.txt ]; then
+if [ ! -e $SPIGOT_HOME/eula.txt ]; then
   if [ "$EULA" != "" ]; then
-    echo "# Generated via Docker on $(date)" > /$SPIGOT_HOME/eula.txt
-    echo "eula=$EULA" >> /$SPIGOT_HOME/eula.txt
+    echo "# Generated via Docker on $(date)" > $SPIGOT_HOME/eula.txt
+    echo "eula=$EULA" >> $SPIGOT_HOME/eula.txt
   else
     echo "*****************************************************************"
     echo "*****************************************************************"
@@ -23,6 +23,11 @@ if [ -z "$REV" ]; then
     REV="latest"
 fi
 
+# Some variables make good shorthand.
+SPIGOT_JENKINS=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild
+ESSENTIALS_JENKINS=https://ci.drtshock.net/job/EssentialsX/lastSuccessfulBuild
+DYNMAP_BUILDS=http://mikeprimm.com/dynmap/builds
+
 # Some variables depend on other variables.
 
 # Creeper block disable is a feature of the Essentials plugin.
@@ -33,42 +38,42 @@ if [ -n "$CREEPERBLOCKDISABLE" ]; then
 fi
 
 # Force rebuild of spigot.jar if REV is latest.
-rm -f /$SPIGOT_HOME/spigot-latest.jar
+rm -f $SPIGOT_HOME/spigot-latest.jar
 
 # Only build a new spigot.jar if a jar for this REV does not already exist.
-if [ ! -f /$SPIGOT_HOME/spigot-$REV.jar ]; then
+if [ ! -f $SPIGOT_HOME/spigot-$REV.jar ]; then
   echo "Building spigot jar file, be patient"
-  mkdir -p /$SPIGOT_HOME/build
-  cd /$SPIGOT_HOME/build
-  wget -O /$SPIGOT_HOME/build/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-  HOME=/$SPIGOT_HOME/build java -jar BuildTools.jar --rev $REV
-  cp /$SPIGOT_HOME/build/Spigot/Spigot-Server/target/spigot-*.jar /$SPIGOT_HOME/spigot-$REV.jar
-  mkdir -p /$SPIGOT_HOME/plugins
+  mkdir -p $SPIGOT_HOME/build
+  cd $SPIGOT_HOME/build
+  wget -O $SPIGOT_HOME/build/BuildTools.jar $SPIGOT_JENKINS/artifact/target/BuildTools.jar
+  HOME=$SPIGOT_HOME/build java -jar BuildTools.jar --rev $REV
+  cp $SPIGOT_HOME/build/Spigot/Spigot-Server/target/spigot-*.jar $SPIGOT_HOME/spigot-$REV.jar
+  mkdir -p $SPIGOT_HOME/plugins
 fi
 
 # Select the spigot.jar for this particular rev.
-rm -f /$SPIGOT_HOME/spigot.jar && ln -s /$SPIGOT_HOME/spigot-$REV.jar /$SPIGOT_HOME/spigot.jar
+rm -f $SPIGOT_HOME/spigot.jar && ln -s $SPIGOT_HOME/spigot-$REV.jar $SPIGOT_HOME/spigot.jar
 
 # Install Dynmap and associated tweaks.
 if [ -n "$DYNMAP" ]; then
   if [ "$DYNMAP" = "true" ]; then
     echo "Downloading Dynmap..."
-    wget -O /$SPIGOT_HOME/plugins/dynmap-HEAD.jar http://mikeprimm.com/dynmap/builds/dynmap/dynmap-HEAD.jar
-    wget -O /$SPIGOT_HOME/plugins/dynmap-mobs-HEAD.jar http://mikeprimm.com/dynmap/builds/dynmap-mobs/dynmap-mobs-HEAD.jar
+    wget -O $SPIGOT_HOME/plugins/dynmap-HEAD.jar $DYNMAP_BUILDS/dynmap/dynmap-HEAD.jar
+    wget -O $SPIGOT_HOME/plugins/dynmap-mobs-HEAD.jar $DYNMAP_BUILDS/dynmap-mobs/dynmap-mobs-HEAD.jar
     if [ -n "$ESSENTIALS" ]; then
       if [ "$ESSENTIALS" = "true" ]; then
         echo "Downloading Dynmap Essentials..."
-        wget -O /$SPIGOT_HOME/plugins/Dynmap-Essentials-HEAD.jar http://mikeprimm.com/dynmap/builds/Dynmap-Essentials/Dynmap-Essentials-HEAD.jar
+        wget -O $SPIGOT_HOME/plugins/Dynmap-Essentials-HEAD.jar $DYNMAP_BUILDS/Dynmap-Essentials/Dynmap-Essentials-HEAD.jar
       else
     echo "Removing Dynmap Essential..."
-        rm -f /$SPIGOT_HOME/plugins/Dynmap-Essentials-HEAD.jar
+        rm -f $SPIGOT_HOME/plugins/Dynmap-Essentials-HEAD.jar
       fi
     fi
   else
     echo "Removing Dynmap..."
-    rm -f /$SPIGOT_HOME/plugins/dynmap-HEAD.jar
-    rm -f /$SPIGOT_HOME/plugins/dynmap-mobs-HEAD.jar
-    rm -f /$SPIGOT_HOME/plugins/Dynmap-Essentials-HEAD.jar
+    rm -f $SPIGOT_HOME/plugins/dynmap-HEAD.jar
+    rm -f $SPIGOT_HOME/plugins/dynmap-mobs-HEAD.jar
+    rm -f $SPIGOT_HOME/plugins/Dynmap-Essentials-HEAD.jar
   fi
 fi
 
@@ -76,18 +81,21 @@ fi
 if [ -n "$ESSENTIALS" ]; then
   if [ "$ESSENTIALS" = "true" ]; then
     echo "Downloading Essentials..."
-    wget -O /$SPIGOT_HOME/plugins/Essentials-2.x-SNAPSHOT.jar https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastStableBuild/artifact/Essentials/target/Essentials-2.x-SNAPSHOT.jar
-    wget -O /$SPIGOT_HOME/plugins/EssentialsProtect-2.x-SNAPSHOT.jar https://hub.spigotmc.org/jenkins/job/Spigot-Essentials/lastStableBuild/artifact/EssentialsProtect/target/EssentialsProtect-2.x-SNAPSHOT.jar
+    ESSENTIALS_JAR=EssentialsX-2.0.1.jar
+    EPROTECT_JAR=EssentialsXProtect-2.0.1.jar
+    wget -O $SPIGOT_HOME/plugins/$ESSENTIALS_JAR $ESSENTIALS_JENKINS/artifact/Essentials/target/$ESSENTIALS_JAR
+    wget -O $SPIGOT_HOME/plugins/$EPROTECT_JAR $ESSENTIALS_JENKINS/artifact/EssentialsProtect/target/$EPROTECT_JAR
     if [ -n "$CREEPERBLOCKDISABLE" ]; then
         if [ "$CREEPERBLOCKDISABLE" = "true" ]; then
-            if [ -f /$SPIGOT_HOME/plugins/Essentials/config.yml ]; then
-                sed -i "s/creeper-blockdamage: .*/creeper-blockdamage: $CREEPERBLOCKDISABLE/" /$SPIGOT_HOME/plugins/Essentials/config.yml
+            if [ -f $SPIGOT_HOME/plugins/Essentials/config.yml ]; then
+                echo "Disabling creeper block damage..."
+                sed -i "s/creeper-blockdamage: .*/creeper-blockdamage: $CREEPERBLOCKDISABLE/" $SPIGOT_HOME/plugins/Essentials/config.yml
             fi
         fi
     fi
   else
     echo "Removing Essentials..."
-    rm -f /$SPIGOT_HOME/plugins/Essentials-2.x-SNAPSHOT.jar
+    rm -f $SPIGOT_HOME/plugins/$ESSENTIALS_JAR
   fi
 fi
 
@@ -95,10 +103,10 @@ fi
 if [ -n "$CLEARLAG" ]; then
   if [ "$CLEARLAG" = "true" ]; then
     echo "Downloading ClearLag..."
-    wget -O /$SPIGOT_HOME/plugins/Clearlag.jar http://dev.bukkit.org/media/files/909/721/Clearlag.jar
+    wget -O $SPIGOT_HOME/plugins/Clearlag.jar http://dev.bukkit.org/media/files/909/721/Clearlag.jar
   else
     echo "Removing Clearlag..."
-    rm -f /$SPIGOT_HOME/plugins/Clearlag.jar
+    rm -f $SPIGOT_HOME/plugins/Clearlag.jar
   fi
 fi
 
@@ -106,107 +114,107 @@ fi
 if [ -n "$PERMISSIONSEX" ]; then
   if [ "$PERMISSIONSEX" = "true" ]; then
     echo "Downloading PermissionsEx..."
-    wget -O /$SPIGOT_HOME/plugins/PermissionsEx-1.23.4.jar http://dev.bukkit.org/media/files/909/154/PermissionsEx-1.23.4.jar
+    wget -O $SPIGOT_HOME/plugins/PermissionsEx-1.23.4.jar http://dev.bukkit.org/media/files/909/154/PermissionsEx-1.23.4.jar
   else
     echo "Removing PermissionsEx..."
-    rm -f /$SPIGOT_HOME/plugins/PermissionsEx-*.jar
+    rm -f $SPIGOT_HOME/plugins/PermissionsEx-*.jar
   fi
 fi
 
 # Install configuration files.
-if [ ! -f /$SPIGOT_HOME/white-list.txt ]
+if [ ! -f $SPIGOT_HOME/white-list.txt ]
 then
-    cp $STATIC_DIR/white-list.txt /$SPIGOT_HOME/
+    cp $STATIC_DIR/white-list.txt $SPIGOT_HOME/
 fi
 
-if [ ! -f /$SPIGOT_HOME/server.properties ]
+if [ ! -f $SPIGOT_HOME/server.properties ]
 then
-  cp $STATIC_DIR/server.properties /$SPIGOT_HOME/
+  cp $STATIC_DIR/server.properties $SPIGOT_HOME/
 fi
 
 # Update configuration file settings.
 if [ -n "$MOTD" ]; then
-  sed -i "/motd\s*=/ c motd=$MOTD" /$SPIGOT_HOME/server.properties
+  sed -i "/motd\s*=/ c motd=$MOTD" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$LEVEL" ]; then
-  sed -i "/level-name\s*=/ c level-name=$LEVEL" /$SPIGOT_HOME/server.properties
+  sed -i "/level-name\s*=/ c level-name=$LEVEL" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$SEED" ]; then
-  sed -i "/level-seed\s*=/ c level-seed=$SEED" /$SPIGOT_HOME/server.properties
+  sed -i "/level-seed\s*=/ c level-seed=$SEED" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$PVP" ]; then
-  sed -i "/pvp\s*=/ c pvp=$PVP" /$SPIGOT_HOME/server.properties
+  sed -i "/pvp\s*=/ c pvp=$PVP" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$VDIST" ]; then
-  sed -i "/view-distance\s*=/ c view-distance=$VDIST" /$SPIGOT_HOME/server.properties
+  sed -i "/view-distance\s*=/ c view-distance=$VDIST" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$OPPERM" ]; then
-  sed -i "/op-permission-level\s*=/ c op-permission-level=$OPPERM" /$SPIGOT_HOME/server.properties
+  sed -i "/op-permission-level\s*=/ c op-permission-level=$OPPERM" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$NETHER" ]; then
-  sed -i "/allow-nether\s*=/ c allow-nether=$NETHER" /$SPIGOT_HOME/server.properties
+  sed -i "/allow-nether\s*=/ c allow-nether=$NETHER" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$FLY" ]; then
-  sed -i "/allow-flight\s*=/ c allow-flight=$FLY" /$SPIGOT_HOME/server.properties
+  sed -i "/allow-flight\s*=/ c allow-flight=$FLY" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$MAXBHEIGHT" ]; then
-  sed -i "/max-build-height\s*=/ c max-build-height=$MAXBHEIGHT" /$SPIGOT_HOME/server.properties
+  sed -i "/max-build-height\s*=/ c max-build-height=$MAXBHEIGHT" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$NPCS" ]; then
-  sed -i "/spawn-npcs\s*=/ c spawn-npcs=$NPCS" /$SPIGOT_HOME/server.properties
+  sed -i "/spawn-npcs\s*=/ c spawn-npcs=$NPCS" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$WLIST" ]; then
-  sed -i "/white-list\s*=/ c white-list=$WLIST" /$SPIGOT_HOME/server.properties
+  sed -i "/white-list\s*=/ c white-list=$WLIST" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$ANIMALS" ]; then
-  sed -i "/spawn-animals\s*=/ c spawn-animals=$ANIMALS" /$SPIGOT_HOME/server.properties
+  sed -i "/spawn-animals\s*=/ c spawn-animals=$ANIMALS" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$HC" ]; then
-  sed -i "/hardcore\s*=/ c hardcore=$HC" /$SPIGOT_HOME/server.properties
+  sed -i "/hardcore\s*=/ c hardcore=$HC" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$ONLINE" ]; then
-  sed -i "/online-mode\s*=/ c online-mode=$ONLINE" /$SPIGOT_HOME/server.properties
+  sed -i "/online-mode\s*=/ c online-mode=$ONLINE" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$RPACK" ]; then
-  sed -i "/resource-pack\s*=/ c resource-pack=$RPACK" /$SPIGOT_HOME/server.properties
+  sed -i "/resource-pack\s*=/ c resource-pack=$RPACK" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$DIFFICULTY" ]; then
-  sed -i "/difficulty\s*=/ c difficulty=$DIFFICULTY" /$SPIGOT_HOME/server.properties
+  sed -i "/difficulty\s*=/ c difficulty=$DIFFICULTY" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$CMDBLOCK" ]; then
-  sed -i "/enable-command-block\s*=/ c enable-command-block=$CMDBLOCK" /$SPIGOT_HOME/server.properties
+  sed -i "/enable-command-block\s*=/ c enable-command-block=$CMDBLOCK" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$MAXPLAYERS" ]; then
-  sed -i "/max-players\s*=/ c max-players=$MAXPLAYERS" /$SPIGOT_HOME/server.properties
+  sed -i "/max-players\s*=/ c max-players=$MAXPLAYERS" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$MONSTERS" ]; then
-  sed -i "/spawn-monsters\s*=/ c spawn-monsters=$MONSTERS" /$SPIGOT_HOME/server.properties
+  sed -i "/spawn-monsters\s*=/ c spawn-monsters=$MONSTERS" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$STRUCTURES" ]; then
-  sed -i "/generate-structures\s*=/ c generate-structures=$STRUCTURES" /$SPIGOT_HOME/server.properties
+  sed -i "/generate-structures\s*=/ c generate-structures=$STRUCTURES" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$SPAWNPROTECTION" ]; then
-  sed -i "/spawn-protection\s*=/ c spawn-protection=$SPAWNPROTECTION" /$SPIGOT_HOME/server.properties
+  sed -i "/spawn-protection\s*=/ c spawn-protection=$SPAWNPROTECTION" $SPIGOT_HOME/server.properties
 fi
 
 if [ -n "$MODE" ]; then
@@ -225,32 +233,32 @@ if [ -n "$MODE" ]; then
       ;;
   esac
 
-  sed -i "/gamemode\s*=/ c gamemode=$MODE" /$SPIGOT_HOME/server.properties
+  sed -i "/gamemode\s*=/ c gamemode=$MODE" $SPIGOT_HOME/server.properties
 fi
 
 # Configure ops file.
-if [ -n "$OPS" -a ! -e /$SPIGOT_HOME/ops.txt.converted ]; then
-  echo $OPS | awk -v RS=, '{print}' >> /$SPIGOT_HOME/ops.txt
+if [ -n "$OPS" -a ! -e $SPIGOT_HOME/ops.txt.converted ]; then
+  echo $OPS | awk -v RS=, '{print}' >> $SPIGOT_HOME/ops.txt
 fi
 
 # Server icon?
-if [ -n "$ICON" -a ! -e /$SPIGOT_HOME/server-icon.png ]; then
+if [ -n "$ICON" -a ! -e $SPIGOT_HOME/server-icon.png ]; then
   echo "Using server icon from $ICON..."
   # Not sure what it is yet...call it "img"
   wget -q -O /tmp/icon.img $ICON
   specs=$(identify /tmp/icon.img | awk '{print $2,$3}')
   if [ "$specs" = "PNG 64x64" ]; then
-    mv /tmp/icon.img /$SPIGOT_HOME/server-icon.png
+    mv /tmp/icon.img $SPIGOT_HOME/server-icon.png
   else
     echo "Converting image to 64x64 PNG..."
-    convert /tmp/icon.img -resize 64x64! /$SPIGOT_HOME/server-icon.png
+    convert /tmp/icon.img -resize 64x64! $SPIGOT_HOME/server-icon.png
   fi
 fi
 
 # Change owner to minecraft.
-chown -R minecraft.minecraft /$SPIGOT_HOME/
+chown -R minecraft.minecraft $SPIGOT_HOME/
 
-cd /$SPIGOT_HOME/
+cd $SPIGOT_HOME/
 
 su - minecraft -c "exec java $JVM_OPTS -jar spigot.jar"
 
